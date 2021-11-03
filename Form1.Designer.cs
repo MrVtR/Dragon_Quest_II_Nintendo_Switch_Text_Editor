e9f4bde50bd52dff0c4a86f0df89818180c31cc5
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-11-03 19:02:48 -0300

    Finished Text Extractor, now I have to add the texts to the TreeView on the code

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace DragonQuestEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.ItemHeight = 16;
+            this.treeView1.Location = new System.Drawing.Point(0, 29);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(307, 423);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(313, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(475, 205);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(313, 240);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(475, 205);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragonQuestEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(new TreeNode("Teste"));
+            treeView1.Nodes[0].Nodes.Add(new TreeNode("Teste Node"));
+            treeView1.EndUpdate();
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string nodetext = e.Node.Text;
+            textBox1.Text = textBox2.Text = nodetext;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(treeView1.SelectedNode == null)
+                    throw new ArgumentNullException(paramName: nameof(treeView1), message: "You need to select an node before start editing...");
+                treeView1.SelectedNode.Text = textBox2.Text;
+            }
+            catch(Exception ex)
+            {
+                textBox2.TextChanged -= textBox2_TextChanged;  // dettach the event handler
+                textBox2.Clear(); // update value
+                textBox2.TextChanged += textBox2_TextChanged; // reattach the event handler
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FontFamily fontfamily = new FontFamily("PRGothIWA-HW-Dm-JReg");
+                int fontSize = 16;
+                Font font = new Font(fontfamily, fontSize, FontStyle.Regular);
+                textBox1.Font = font;
+                textBox2.Font = font;
+                textBox2.Enabled = true;
+
+                Console.WriteLine("DEBUG:");
+                string path = openFileDialog.FileName;
+                var textsArray = new ArrayList();
+
+                byte[] readText = File.ReadAllBytes(path);
+                int i = 2;
+
+
+                while (i + 1 < readText.Length)
+                {
+                    string testing = readText[i + 6].ToString("X");
+                    // 2 Arrays
+                    if (testing.Equals("3C")|| testing.Equals("30") || testing.Equals("31"))
+                    {
+                        string master1 = readText[i].ToString("X");
+                        string master2 = readText[i + 1].ToString("X");
+                        if (master1.Equals("0"))
+                            master1 = "00";
+                        if (master2.Equals("0"))
+                            master2 = "00";
+                        string masterArrayLength = master1 + master2;
+                        i += 2;
+                        Console.WriteLine("Tamanho do array Mestre: " + Convert.ToInt32(masterArrayLength, 16) + " " + i+ " "+testing);
+                        for (int j = 0; j < Convert.ToInt32(masterArrayLength, 16); j++)
+                        {
+                            string sub1 = readText[i].ToString("X");
+                            string sub2 = readText[i + 1].ToString("X");
+                            if (sub1.Equals("0"))
+                                sub1 = "00";
+                            if (sub2.Equals("0"))
+                                sub2 = "00";
+                            string subArrayLength = sub1 + sub2;
+                            i += 2;
+                            Console.WriteLine("\tTamanho do sub array: " + Convert.ToInt32(subArrayLength, 16) + " " + i);
+                            for (int k = 0; k < Convert.ToInt32(subArrayLength, 16); k++)
+                            {
+                                string text1 = readText[i].ToString("X");
+                                string text2 = readText[i + 1].ToString("X");
+                                if (text1.Equals("0"))
+                                    text1 = "00";
+                                if (text2.Equals("0"))
+                                    text2 = "00";
+                                string textStringLength = text1 + text2;
+                                Console.WriteLine("\t\tTamanho dos textos: " + textStringLength + " " + i + "\n");
+                                int cont = i + 2;
+                                var byteText = new ArrayList();
+                                for (int l = 0; l < Convert.ToInt32(textStringLength, 16); l++)
+                                {
+                                    byteText.Add(readText[cont++]);
+                                }
+                                byte[] text = new byte[byteText.Count];
+                                int cont2 = 0;
+                                foreach (byte b in byteText)
+                                {
+                                    text[cont2] = b;
+                                    cont2++;
+                                }
+                                string currentText = System.Text.Encoding.UTF8.GetString(text);
+                                Console.WriteLine(currentText);
+                                i += Convert.ToInt32(textStringLength, 16) + 3;
+                            }
+                            j += Convert.ToInt32(subArrayLength, 16) - 1;
+                        }
+                    }
+                    // 1 Array
+                    else 
+                    {
+                        string master1 = readText[i].ToString("X");
+                        string master2 = readText[i + 1].ToString("X");
+                        if (master1.Equals("0"))
+                            master1 = "00";
+                        if (master2.Equals("0"))
+                            master2 = "00";
+                        string masterArrayLength = master1 + master2;
+                        i += 2;
+                        Console.WriteLine("Tamanho do array Mestre: " + Convert.ToInt32(masterArrayLength, 16) + " " + i + " " + testing);
+                        for (int j = 0; j < Convert.ToInt32(masterArrayLength, 16); j++)
+                        {
+
+                            string text1 = readText[i].ToString("X");
+                            string text2 = readText[i + 1].ToString("X");
+                            if (text1.Equals("0"))
+                                text1 = "00";
+                            if (text2.Equals("0"))
+                                text2 = "00";
+                            string textStringLength = text1 + text2;
+                            Console.WriteLine("\t\tTamanho dos textos: " + textStringLength + " " + i + "\n");
+                            int cont = i + 2;
+                            var byteText = new ArrayList();
+                            for (int l = 0; l < Convert.ToInt32(textStringLength, 16); l++)
+                            {
+                                byteText.Add(readText[cont++]);
+                            }
+                            byte[] text = new byte[byteText.Count];
+                            int cont2 = 0;
+                            foreach (byte b in byteText)
+                            {
+                                text[cont2] = b;
+                                cont2++;
+                            }
+                            string currentText = System.Text.Encoding.UTF8.GetString(text);
+                            Console.WriteLine(currentText);
+                            i += Convert.ToInt32(textStringLength, 16) + 3;
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://google.com");
+        }
+    }
+}
